8a4be6b60e8ac0033c4dea76a2355bb418e5d495
Locki Lo <locki2568@gmail.com>
2018-11-18 22:53:16 +0800

    Completed all features

--- js/todo.js
+++ js/todo.js
@@ -23,24 +23,26 @@ $(document)
 
         function generateNewOrderListElement(inputValue){
             var uuid = generateUUID();
-            var newLiElement = '<li id="'+uuid+'" class="123"><input name="done-todo" type="checkbox" class="done-todo">'+inputValue+'</li>';
+            var newLiElement = '<li id="'+uuid+'" class="listElement"><input name="done-todo" type="checkbox" class="done-todo">'+inputValue+'</li>';
             $("ol").append(newLiElement);
         }
 
         $("div#button").click(function(){
             var inputValue = $("input.input-text").val();
-            generateNewOrderListElement(inputValue);
-            $("input.input-text").val("");
+            if (inputValue.length > 0){
+                generateNewOrderListElement(inputValue);
+                $("input.input-text").val("");
+            }
         });
 
-        $("input.done-todo").change(function(){
-            //var input = $(this).val();
-            console.log(this)
-            console.log(this.checked)
-            if(this.checked){
-                $("input.done-todo").css('textDecoration', 'line-through');
-            }
-        })
+        // $("input.done-todo").change(function(){
+        //     //var input = $(this).val();
+        //     console.log(this)
+        //     console.log(this.checked)
+        //     if(this.checked){
+        //         $("input.done-todo").css('textDecoration', 'line-through');
+        //     }
+        // })
 
         
         $(document).on("change", ":checkbox", function () {
@@ -50,8 +52,53 @@ $(document)
                 $(this).parent().removeClass('checked');
             }
         });
-        // $("ol li").attr("id", generateUUID());
-        // alert($("li.123").attr("id"));
+        
+        $(".complete").click(function(){
+            console.log("complete");
+            this.focus();
+            var listItems = $(".listElement");
+            listItems.each(function( index, li ) {
+                // Show all the item for condition checking
+                $(this).show();
+                console.log("complete.each");
+                var item = $(li);
+                if(!$(this).find('input').is(':checked')){
+                    $(this).hide();
+                }
+            });
+        });
+
+        $(".active").click(function(){
+            console.log("active");
+            this.focus();
+            var listItems = $(".listElement");
+            listItems.each(function( index, li ) {
+                console.log("active.each");
+                // Show all the item for condition checking
+                $(this).show();
+                var item = $(li);
+                if($(this).find('input').is(':checked')){
+                    $(this).hide();
+                }
+            });
+        });
+
+        $(".all").click(function(){
+            console.log("all");
+            this.focus();
+            var listItems = $(".listElement");
+            listItems.each(function( index, li ) {
+                console.log("all.each");
+                var item = $(li);
+                $(this).show();
+            });
+        });
+
+        // var listItems = $("#productList li");
+        // listItems.each(function(idx, li) {
+        // var product = $(li);
+        // });
+        // // and the rest of your code
 
         // code to be implemented
         
